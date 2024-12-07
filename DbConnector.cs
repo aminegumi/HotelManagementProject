37677c6fda19271c9a885350351d38f6e7d6adb7
Mohammed  Amine Agoumi <agoumi2002@gmail.com>
2024-12-07 16:55:38 +0100

    login done

---
+++ DbConnector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace HotelRes1
+{
+    internal class DbConnector
+    {
+        private string _connectionString;
+        private MySqlConnection connection;
+        public DbConnector() {
+            _connectionString = "Server=127.0.0.1;Database=hotel_management_system;Uid=root;Pwd=;";
+            connection = new MySqlConnection(_connectionString);
+        }
+
+
+        public bool IsValidNamePass(string username, string password)
+        {
+            try
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM user_table WHERE User_Name = @username AND User_Password = @password";
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    int result = Convert.ToInt32(cmd.ExecuteScalar());
+                    return result > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
